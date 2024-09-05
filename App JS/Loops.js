0aab3ec089ec550e7d0f2252f979349a73b67813
Bearbrand-sy <sanderperejan@gmail.com>
2024-09-05 11:47:09 +0800

    "added app.js"

---
+++ App JS/Loops.js
@@ -0,0 +1,20 @@
+//Loops
+//for loop
+
+for(let i=0; i < 5; i++ ){
+    console.log("The numbers are: " + i);
+}
+
+//while loop 
+let loop = 0;
+   while(loop < 6){
+    loop++;
+   }
+console.log("The numbers are: " + loop);
+
+//do while loop
+let s = 1;
+ do {
+    console.log("The numbers are: " + s);
+    s++;
+ }while (s < 5);
